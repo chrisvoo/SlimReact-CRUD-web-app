04e4ea6f93ee1994c7ce2e02da8e7be3a2ee0fcc
Christian Castelli <voodoo81people@gmail.com>
2021-02-03 09:41:55 +0100

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-rsync -av -e ssh --exclude='api/vendor' api ccastelli@localhost:/var/www/chessable
-ssh ccastelli@localhost 'cd /var/www/chessable && composer install'
-cd ui
-npm install
-npm run build
-scp -r build/* ccastelli@localhost:/var/www/chessable
